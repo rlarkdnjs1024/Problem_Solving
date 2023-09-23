f05dfe69067fa1d83a3590524b61b30de8a0ce7c
김가원 <122495332+rlarkdnjs1024@users.noreply.github.com>
2023-09-23 22:11:47 +0900

    Update 단반향_연결리스트.c
    코드 수정

--- 자료구조/단반향_연결리스트.c
+++ 자료구조/단반향_연결리스트.c
@@ -1,117 +1,115 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef int data;
 
 typedef struct _listNode
 {
-    int element;
+    data element;
     struct _listNode *next;
+    
 }listNode;
 
-void printList (listNode *head);
-listNode *insertFirst (listNode *head, int element);
-listNode *deletefirst (listNode *head);
-listNode *insertLast (listNode *head, int element);
+typedef struct
+{
+    int size;
+    listNode *headAdrs;
+    listNode *lastAdrs;
+    
+}list;
+
+void initList (list *p)
+{
+    p->headAdrs = NULL;
+    p->lastAdrs = NULL;
+    p->size = 0;
+}
+
+void printList (list *p)
+{
+    listNode *temp = p->headAdrs; //첫번째 메모리의 주소를 저장한다.
+    while (temp != NULL)
+    {
+        printf("%d ",temp->element);
+        temp = temp->next; //다음 주소를 저장한다.
+    }
+    printf("\n");
+}
+
+void insertFirst (list *p, data element)
+{
+    listNode *temp = (listNode *)malloc(sizeof(listNode));
+    if (p->headAdrs == NULL) //리스트가 비어 있었을 떄
+    {
+        temp->element = element;
+        temp->next = NULL;
+        p->headAdrs = temp;
+        p->lastAdrs = temp;
+       
+    }
+    else
+    {
+        temp->element = element;
+        temp->next = p->headAdrs;
+        p->headAdrs = temp;
+    }
+    p->size++;
+}
+
+void deleteFirst (list *p)
+{
+    listNode *temp;
+    
+    if (p->headAdrs == NULL) //리스트가 아예 비어있을떄
+        return;
+    
+    
+    temp = p->headAdrs->next;
+    
+    if (temp == NULL) //리스트에 원소가 하나 있었을때
+    {
+        free(p->headAdrs);
+        p->headAdrs = NULL;
+        p->lastAdrs = NULL;
+    }
+    
+    else //나머지 보통의 경우
+    {
+        free(p->headAdrs);
+        p->headAdrs = temp;
+        
+    }
+    p->size--;
+}
 
 int main(void)
 {
-    listNode *head = NULL;
-    while(1)
-    {
-        int menu;
-        int temp;
-        printf("\n<메뉴를 선택하세요>\n 1: 리스트 출력\n 2: 맨 앞에 추가\n 3 : 맨 뒤에 추가\n 4 : 맨 앞 제거\n 5: 종료\n->");
-        scanf("%d",&menu);
-        if (menu == 0)
-        {
-            printList(head);
-        }
-        else if (menu == 1)
-        {
-            printList(head);
-        }
-        
-        else if (menu == 2)
-        {
-            printf("정수를 입력하세요 ->");
-            scanf("%d",&temp);
-            head = insertFirst(head, temp);
-        }
-        else if (menu == 3)
-        {
-            printf("정수를 입력하세요 ->");
-            scanf("%d",&temp);
-            head = insertLast(head, temp);
-        }
-        else if (menu == 4)
-        {
-            head = deletefirst(head);
-        }
-        else if (menu == 5)
-        {
-            return 0;
-        }
-    }
-}
+    list a;
+    initList(&a);
     
-void printList (listNode *head)
-{
-    if (head == NULL)
-        printf("리스트가 비어있습니다.\n");
-    else
-    {
-        for (listNode *p = head; p != NULL; p = p->next) //p가 현재 출력할 노드를 가르킨다. 출력 후, 다음 노드를 가르킨다.
-        {
-            printf("%d",p->element);
-            if (p->next != NULL)
-                printf(" -> ");
-        }
-        printf("\n");
-    }
+    deleteFirst(&a);
+    printList(&a);
+    
+    insertFirst(&a, 10);
+    printList(&a);
+    
+    deleteFirst(&a);
+    printList(&a);
+    
+    insertFirst(&a, 20);
+    printList(&a);
+    
+    insertFirst(&a, 30);
+    printList(&a);
+    
+    insertFirst(&a, 40);
+    printList(&a);
+    
+    deleteFirst(&a);
+    printList(&a);
+    
+    insertFirst(&a, 50);
+    printList(&a);
+    
+    return 0;
 }
-
-listNode *insertFirst (listNode *head, int element)
-{
-    listNode *new = (listNode *)malloc(sizeof(listNode)); //새로운 노드를 생성하고, 그 노드의 주소를 저장한다.
-    new->element = element;
-    new->next = head; //우선 가장 앞에 올 노드를 두번째 노드에 연결한다.
-    head = new;
-    return head;
-}
-
-listNode *deletefirst (listNode *head)
-{
-    if (head == NULL)
-    {
-        printf("리스트가 비어있습니다.\n");
-        return NULL;
-    }
-    listNode *rm = head;
-    head = rm->next;
-    free(rm);
-    return head;
-}
-
-listNode *insertLast (listNode *head, int element)
-{
-    listNode *new = malloc(sizeof(listNode));
-    new->element = element;
-    new->next = NULL;
-    listNode *p = head;
-    if (head == NULL)
-    {
-        head = new;
-    }
-    else
-    {
-        while(p->next != NULL)
-        {
-            p = p->next;
-        }
-        p->next = new;
-    }
-    return head;
-}
-
-
-
